f96290628fc65eb07f55a82e2c898f86d57527ef
khadijah-05 <khadija786.02005@gmail.com>
2024-10-28 23:13:45 +0400

    Cool is Cooling

---
+++ Exercise 01/Coding is Cool.py
@@ -0,0 +1,8 @@
+# Exercise 1: Coding is Cool
+
+word1 = "Coding" #The first word is assigned to the variable word1
+word2 = "is" #The second word is assigned to the variable word2
+word3 = "Cool" #The third word is assigned to the variable word3
+
+# Combining and printing the statement via string concatenation
+print (word1 + " " + word2 + " "+ word3)
