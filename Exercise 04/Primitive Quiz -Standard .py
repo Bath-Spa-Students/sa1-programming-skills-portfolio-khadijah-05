bb1d3de2ae45aea603eaeb6ba00103ec1f12e4b2
khadijah-05 <khadija786.02005@gmail.com>
2024-11-07 20:21:41 +0400

    Primitive Quiz -Standard

--- Exercise 04/Primitive Quiz -Standard .py
+++ Exercise 04/Primitive Quiz -Standard .py
@@ -1,20 +1,12 @@
-# Exercise 2: Simple Sums
+#Exercise 4: Primitive Quiz
 
-# Delcaring a variable and initialing it with the integer value 8
-number1 = 8
-
-# Declaring a second variable and initialing it with the integer value 10.
-number2 = 10 
-
-# Declaring a third variable that stores the sum of first two numbers.
-sum_result = number1 + number2
-
-# Printing the value of the sum to the console.
-print("Sum of 8 and 10 is", sum_result)#Writing a program that asks the user "What is the capital of France?".
+#Writing a program that asks the user "What is the capital of France?".
 question = input ("What is the capital of France?")
+
 # If the answer is correct (i.e., "Paris"), printing a message saying the answer is correct.
-if question.lower() == "Paris":
+if question.lower() == "paris":
     print ("Correct")
+
 #If the answer is incorrect, printing a message saying the answer is wrong.
 else: 
     print("Wrong")
