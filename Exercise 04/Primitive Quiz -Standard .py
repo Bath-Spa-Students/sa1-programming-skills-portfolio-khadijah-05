55e5823c9e08b1d179e2fc99de5139b006fe1d89
khadijah-05 <khadija786.02005@gmail.com>
2024-11-07 10:20:56 +0400

    Primitive Quiz -Standard

--- Exercise 04/Primitive Quiz -Standard .py
+++ Exercise 04/Primitive Quiz -Standard .py
@@ -10,4 +10,11 @@ number2 = 10
 sum_result = number1 + number2
 
 # Printing the value of the sum to the console.
-print("Sum of 8 and 10 is", sum_result)
+print("Sum of 8 and 10 is", sum_result)#Writing a program that asks the user "What is the capital of France?".
+question = input ("What is the capital of France?")
+# If the answer is correct (i.e., "Paris"), printing a message saying the answer is correct.
+if question.lower() == "Paris":
+    print ("Correct")
+#If the answer is incorrect, printing a message saying the answer is wrong.
+else: 
+    print("Wrong")
