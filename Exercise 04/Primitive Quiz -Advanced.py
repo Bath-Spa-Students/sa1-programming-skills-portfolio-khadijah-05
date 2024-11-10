12cc6adb19597ae7cab00c7c95df618a78b2ce8c
khadijah-05 <khadija786.02005@gmail.com>
2024-11-10 18:27:37 +0400

    Primitive Quiz -Advanced

--- Exercise 04/Primitive Quiz -Advanced.py
+++ Exercise 04/Primitive Quiz -Advanced.py
@@ -1,1 +1,35 @@
+#Advanced Requirements
 
+#Extending the program into a quiz that asks for the capitals of 10 European countries.
+
+#Defining the function.
+def quiz():    
+ 
+ #Storing the information in the dictionary.
+ question = {
+
+    "What is the capital of France?": "Paris",
+    "What is the capital of Germany?": "Berlin",
+    "What is the capital of Italy?": "Rome",
+    "What is the capital of Spain?": "Madrid",
+    "What is the capital of United Kingdom?": "London",
+    "What is the capital of Portugal?": "Lisbon",
+    "What is the capital of Netherlands?": "Amsterdam",
+    "What is the capital of Belgium?": "Brussels",
+    "What is the capital of Sweden?": "Stockholm",
+    "What is the capital of Norway?": "Oslo"
+} 
+ for question, answer in question.items():
+    
+    #Using .strip() to accept answers regardless of the capitalization.
+    user_answer = input(question + " " ).strip()
+    
+    if user_answer.lower() == answer.lower():
+
+        #Checking if the user's answer is in correct, then printing correct.
+        print("Correct")
+
+        #If not correct, printing wrong.
+    else:
+        print ("Wrong")
+quiz()
