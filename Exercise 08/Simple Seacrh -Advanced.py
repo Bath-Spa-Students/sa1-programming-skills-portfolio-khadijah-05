06639500ca96b41add5a1a9f685bb1432b11dcf2
khadijah-05 <khadija786.02005@gmail.com>
2024-11-15 18:44:46 +0400

    Simple Search -Advanced

---
+++ Exercise 08/Simple Seacrh -Advanced.py
@@ -0,0 +1,13 @@
+# Optional Requirements
+
+#The list is initialized with specific names ("Jake" "Zac", "Ian", "Ron", "Sam", "Dave"). ,
+list_of_the_name = ["Jake", "Zac", "Ian", "Ron", "Sam", "Dave"] 
+
+#Making a string for "Sam".
+searching_for_a_name  = input("Enter the name you are searchinf for: ")
+
+#Using if-else statement to search for "Sam".
+if searching_for_a_name in list_of_the_name:
+    print(f"{searching_for_a_name} found in the list.")
+else:
+    print(f"{searching_for_a_name} not found in the list.")
