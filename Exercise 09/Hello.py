46fa86f73fe4413b525f6d690fe9b706ba50b535
khadijah-05 <khadija786.02005@gmail.com>
2024-11-17 17:20:42 +0400

    Hello

--- Exercise 09/Hello.py
+++ Exercise 09/Hello.py
@@ -2,11 +2,12 @@
 
 #Fill in the blanks in the code below so that the function hello() prints "Hello" to the console.
 
+#Filling this blank to print "Hello" to the console.
 def hello():
- print ("Hello")  # Filling this blank to print "Hello" to the console.
+ print ("Hello")  
 
+#Filling in this blank to call the hello() function.
 def main():
-    hello()  # Filling in this blank to call the hello() function
-
+    hello()  
 if __name__ == "__main__":
     main()
