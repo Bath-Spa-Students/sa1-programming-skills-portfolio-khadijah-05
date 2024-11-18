c88c4c0fd1a30bb190237e35f1e2abc0488a8948
khadijah-05 <khadija786.02005@gmail.com>
2024-11-18 20:09:46 +0400

    Simple Sum

--- Exercise 02/Simple Sum.py
+++ Exercise 02/Simple Sum.py
@@ -1,13 +1,13 @@
 # Exercise 2: Simple Sums
 
 # Delcaring a variable and initialing it with the integer value 8
-number1 = 8
+first_number = 8
 
 # Declaring a second variable and initialing it with the integer value 10.
-number2 = 10 
+second_number = 10 
 
 # Declaring a third variable that stores the sum of first two numbers.
-sum_result = number1 + number2
+sum_of_these_numbers = first_number + second_number 
 
 # Printing the value of the sum to the console.
-print("Sum of 8 and 10 is", sum_result)
+print("Sum of 8 and 10 is", sum_of_these_numbers)
