48ca669c8028c1627292ecad22796332ac10d617
khadijah-05 <khadija786.02005@gmail.com>
2024-11-18 09:43:17 +0400

    Coding is Cool

--- Exercise 01/Coding is Cool.py
+++ Exercise 01/Coding is Cool.py
@@ -5,7 +5,7 @@
 #Assiging each word to the respective varaible.
 word1 = "Coding" 
 word2 = "is" 
-word3 = "Cool" #
+word3 = "Cool" 
 
 #Combining and printing the statement via string concatenation.
 print (word1 + " " + word2 + " "+ word3)
