110f1f78afda91fe3d333a6a4cfe82ea47fb64df
khadijah-05 <khadija786.02005@gmail.com>
2024-11-18 20:33:04 +0400

    Standard

--- Exercise 05/Days of the Month -Standard.py
+++ Exercise 05/Days of the Month -Standard.py
@@ -5,13 +5,13 @@ days_of_the_month = {
     1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31
 }
 #Asking the user to input the month number.
-month = int(input("Enter the month number(1-12):"))
+month_number = int(input("Enter the month number(1-12):"))
 
 #Using an if-else statement.
-if 1 <= month <=12:
+if 1 <= month_number <=12:
     
     #If the input is valid than printing the number of days in the corresponding month.
-    print(f"The number of days in month {month} is {days_of_the_month[month]}.")
+    print(f"The number of days in month {month_number} is {days_of_the_month[month_number]}.")
     
 #For invalid month number, Asking the user to provide a valid month number.
 else:
