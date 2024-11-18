942de1aea94d89689c887a3532a35fca3fb0a28b
khadijah-05 <khadija786.02005@gmail.com>
2024-11-18 20:44:43 +0400

    Some Counting

--- Exercise 07/Some Counting.py
+++ Exercise 07/Some Counting.py
@@ -5,46 +5,32 @@ print("Counting up from 0 to 50 in increments of 1:")
 
 #Ranges from 0 to 50, incrementing 1.
 for a in range(0, 51): 
-
-    #To start a new line.
- print("\n")  
+    print(a)
 
 #Writing a loop that counts down from 50 to 0 in decrements of 1.
 print("Counting down from 50 to 0 in decrements of 1:")
 
 #Ranges from 50 to 0, decrementing -1.
 for b in range(50, -1, -1):  
-    print(b, end=" ")
-
-    #To start a new line.
-print("\n")
+    print(b)
 
 #Writing a loop that counts up from 30 to 50 in increments of 1.
 print("Counting up from 30 to 50 in increments of 1:")
 
 #Ranges from 30 to 50, incrementing 1.
 for c in range(30, 51):   
-    print(c, end=" ")
-
-    #To start the new line.
-print("\n")
+    print(c)
 
 #Writing a loop that counts down from 50 to 10 in decrements of 2.
 print("Counting down from 50 to 10 in decrements of 2:")
 
 #Ranges from 50 to 10, decrementing -2.
 for d in range(50, 9, -2):  
-    print(d, end=" ")
-
-    #To start the new line.
-print("\n")
+    print(d)
 
 #Writing a loop that counts up from 100 to 200 in increments of 5.
 print("Counting up from 100 to 200 in increments of 5:")
 
 #Ranges from 100 to 200, incrementing 5.
 for e in range(100, 201, 5):  
-    print(e, end=" ")
-    
-    #To start the new line.
-print("\n")
+    print(e)
