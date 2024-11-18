a283fbc98906ab961496a2c8a44173028fb36cc1
khadijah-05 <khadija786.02005@gmail.com>
2024-11-18 20:17:40 +0400

    Standard

--- Exercise 03/Biography -Standard.py
+++ Exercise 03/Biography -Standard.py
@@ -1,8 +1,8 @@
 # Exercise 3: Biography 
 
 #Storing the information (name, hometown, and age) as key-value pairs in a dictionary.
-dictionary = {
-    
+storing_the_information = {
+
     #Using variables with appropriate data types for each piece of information.
     "name": "Khadeeja",
     "hometown": "Ajman",
@@ -10,4 +10,4 @@ dictionary = {
 }
 
 #Printing the values on separate lines using a single `print()` statement.
-print(f"Name: {dictionary['name']}\nHometown: {dictionary['hometown']}\nAge: {dictionary['age']}")
+print(f"Name: {storing_the_information['name']}\nHometown: {storing_the_information['hometown']}\nAge: {storing_the_information['age']}")
