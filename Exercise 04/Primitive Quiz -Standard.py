ad4d4e15964ab839f1f622ea61a6e2cd50b5ce40
khadijah-05 <khadija786.02005@gmail.com>
2024-11-18 20:15:15 +0400

    Standard

--- Exercise 04/Primitive Quiz -Standard.py
+++ Exercise 04/Primitive Quiz -Standard.py
@@ -1,12 +1,12 @@
 #Exercise 4: Primitive Quiz
 
 #Writing a program that asks the user "What is the capital of France?".
-question = input ("What is the capital of France?")
+asking_the_user = input ("What is the capital of France?")
 
 #If the answer is correct (i.e., "Paris"), printing a message saying the answer is correct.
-if question.lower() == "paris":
-    print ("Correct")
+if asking_the_user.lower() == "paris":
+    print ("Correct Answer!")
 
 #If the answer is incorrect, printing a message saying the answer is wrong.
 else: 
-    print("Wrong")
+    print("Incorrect Answer!")
