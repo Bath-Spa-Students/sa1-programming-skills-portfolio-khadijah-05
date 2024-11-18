2e0f293052ad0fd69ca11280ca107b6ce8758308
khadijah-05 <khadija786.02005@gmail.com>
2024-11-18 20:35:59 +0400

    Standard

--- Exercise 06/Brute Force Attack -Standard.py
+++ Exercise 06/Brute Force Attack -Standard.py
@@ -1,13 +1,13 @@
 #Exercise 6: Brute Force Attack 
 
 #Defining the correct password.
-password = "12345"
+correct_password = "12345"
 
 #Asking the user to enter the password.
 asking_the_user_to_enter_the_password = " "
 
 #Using a while loop to repeatedly ask the user for the password until the correct one is entered.
-while asking_the_user_to_enter_the_password != password:
+while asking_the_user_to_enter_the_password != correct_password:
     asking_the_user_to_enter_the_password = input("Enter the password: ")
 
 #Output an appropriate message when the correct password is entered.
