71ad919d95ea2f0779e730bf47bd60fce06538ca
khadijah-05 <khadija786.02005@gmail.com>
2024-11-19 15:07:26 +0400

    Dictionaries

---
+++ Bonus Exercise/Dictionaries.py
@@ -0,0 +1,15 @@
+#Using a dictionary to store information about a person you know.
+
+#Storing their first name,last name, age, and the city in which they live.
+personal_information = {
+    'first_name' : 'Khadeeja',
+    'last_name' : 'Maheen',
+    'age' : '18',
+    'city' : 'Ajman'
+}
+
+#Having keys such as first_name, last_name, age, and city is Printed each piece of information stored in your dictionary.
+print("First Name:", personal_information['first_name'])
+print("Last Name:", personal_information['last_name'])
+print("Age:", personal_information['age'])
+print("City:", personal_information['city'])
