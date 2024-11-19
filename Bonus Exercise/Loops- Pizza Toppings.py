9a11694cf15cf69aed4d2320398f2e466e6e789d
khadijah-05 <khadija786.02005@gmail.com>
2024-11-19 15:08:41 +0400

    Loops- Pizza Toppings

---
+++ Bonus Exercise/Loops- Pizza Toppings.py
@@ -0,0 +1,10 @@
+#Writing a loop that prompts the user to enter a series of pizza toppings. 
+while True:
+    topping_for_pizza = input("Enter a topping for your pizza or type 'quit' to quit adding topping: ")
+
+    #Until they enter a'quit' value. 
+    if topping_for_pizza.lower() == 'quit':
+        break
+    
+    #As they enter each topping, Printing a message saying you’ll add that topping to their pizza.
+    print(f"Adding {topping_for_pizza} to your pizza.")
