08e245809f16484374536966631ba44f21bee928
khadijah-05 <khadija786.02005@gmail.com>
2024-11-19 15:06:58 +0400

    List -Names

---
+++ Bonus Exercise/List -Names.py
@@ -0,0 +1,6 @@
+#Storing the names of a few of your friends in a list called names. 
+names_of_my_friends =["Ali", "Ahmed","Muhammad"]
+
+#Printing each person’s name by accessing each element in the list, one at a time.
+for name in names_of_my_friends:
+    print(name)
