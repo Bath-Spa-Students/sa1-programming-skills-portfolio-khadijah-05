79e6e127a63168630b7d02cf925e0572c7444600
khadijah-05 <khadija786.02005@gmail.com>
2024-11-19 14:29:45 +0400

    Advanced

---
+++ Exercise 03/Biography -Advanced.py
@@ -0,0 +1,16 @@
+#Advanced Requirements
+
+#Asking the user input their name, hometown, and age.
+
+#Ensuring multiple words can be entered for name.
+name = input("Enter your full name: ")  
+hometown = input("Enter your hometown: ")
+
+#Ensuring age is valid.
+while True:
+ age = input("Enter your age: ")
+ if not age.isdigit():
+   print ("Invalid, please enter a number for age.")
+ else:
+   age = int(age)
+   break
