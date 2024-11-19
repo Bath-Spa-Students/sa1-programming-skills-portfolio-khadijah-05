73edf25303df5e693934f6fb07eaed252b2671e8
khadijah-05 <khadija786.02005@gmail.com>
2024-11-19 15:09:49 +0400

    While Loop

---
+++ Class Activity/Loops/While Loop.py
@@ -0,0 +1,13 @@
+#In while loops as long as condition is true, we can use the while loop to run a series of statements.
+a = 1
+while a < 5: #Program will print a until it is less tha 5. 
+  print(a)
+  a += 1  # a =a+1 
+  
+#While loop with a statement.
+b = 1
+while b < 5:
+  print(b)
+  if b == 3: #Program will print until it is equal to 3.
+    break
+  b += 1
