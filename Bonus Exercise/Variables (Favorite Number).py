2549607ddf8e5aa79663820c834b2c473b5d1943
khadijah-05 <khadija786.02005@gmail.com>
2024-11-19 15:05:57 +0400

    Variables (Favorite Number)

---
+++ Bonus Exercise/Variables (Favorite Number).py
@@ -0,0 +1,8 @@
+#Using a variable to represent your favorite number.
+favorite_number = 7
+
+#using that variable, creating a message that reveals your favorite number. 
+message = str(favorite_number) + " is my favorite number."
+
+#Printing that message.
+print(message)
