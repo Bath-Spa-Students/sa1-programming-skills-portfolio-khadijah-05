6ae7870cd7a94024048effbe9b9db101c373162e
khadijah-05 <khadija786.02005@gmail.com>
2024-11-19 15:04:26 +0400

    Variables

---
+++ Bonus Exercise/Variables.py
@@ -0,0 +1,7 @@
+#Assigning  a message to a variable, and printing that message. 
+message = "Khadeeja"
+print(message)
+
+#Changing the value of the variable to a new message, and printing the new message.
+message = "Maheen"
+print(message)
