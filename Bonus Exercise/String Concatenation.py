a72c1ac941ae23ede86fc6e1d3264982dfa471b8
khadijah-05 <khadija786.02005@gmail.com>
2024-11-19 15:03:18 +0400

    String Concatenation

---
+++ Bonus Exercise/String Concatenation.py
@@ -0,0 +1,9 @@
+#Writing three strings in different variables and printing the output as one string.
+
+#Defining the three string variables.
+word1 = "Code"
+word2 = "Lab"
+word3 = "1"
+
+#Combining and printing the statement via string concatenation
+print (word1 + " " + word2 + " "+ word3)
