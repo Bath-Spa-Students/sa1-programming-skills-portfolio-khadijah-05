79828f37ca8ea3a9cb189ca2d303111ee2c27433
khadijah-05 <khadija786.02005@gmail.com>
2024-11-21 17:32:06 +0400

    Lists

--- Class Activity/Lists/Lists.py
+++
@@ -1,9 +0,0 @@
-#Basic List.
-basic_list = ["Khadeeja", "Maheen"]
-
-#Homogeneous List.
-homogeneous_list =['1', "2", "3", "4", "5"]
-
-#hetrogeneous List.
-hetrogeneous_list = ["Khadeeja", 123, 789.0]
-
