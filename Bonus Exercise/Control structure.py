213683db9a82c99f4c0a26af8528d0aae2abff8f
khadijah-05 <khadija786.02005@gmail.com>
2024-11-21 22:43:29 +0400

    Control Structure

--- Bonus Exercise/Control structure.py
+++ Bonus Exercise/Control structure.py
@@ -1,13 +1,13 @@
 #Imagine an alien was just shot down in a game. 
 
 #Creating a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
-alien_color = 'green'
+alien_color = 'red'
 
 #Writing an if statement to test whether the alien’s color is green. If it is, printing a message that the player just earned 5 points.
-if alien_color == "green":
+if alien_color == "red":
     print ("You earned 5 point.")
 
 #Writing a version of this program that passes the if test and another that fails. The version that fails will have no output.
-alien_color = 'red'
+alien_color = 'yellow'
 if alien_color == "green":
     print ("You earned 5 point.")
