1d2ddc6157b69bb77035343e16e5eb462a1a222b
khadijah-05 <khadija786.02005@gmail.com>
2024-11-21 19:29:10 +0400

    Optional

--- Exercise 08/Simple Seacrh -Optional.py
+++ Exercise 08/Simple Seacrh -Optional.py
@@ -3,10 +3,10 @@
 #The list is initialized with specific names ("Jake" "Zac", "Ian", "Ron", "Sam", "Dave"). 
 list_of_the_name = ["Jake", "Zac", "Ian", "Ron", "Sam", "Dave"] 
 
-#Making a string for "Sam".
+#Making a string that looks for the entered name.
 searching_for_a_name  = input("Enter the name you are searching for: ")
 
-#Using if-else statement to search for "Sam".
+#Using if-else statement to search.
 if searching_for_a_name in list_of_the_name:
     print(f"{searching_for_a_name} found in the list.")
 else:
